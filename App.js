913eb2f035b035755270a0baa59c39507b98bd08
radin-reth <radin.reth@gmail.com>
2019-09-12 21:41:43 +0700

    add basic todo

--- App.js
+++ App.js
@@ -6,19 +6,53 @@
  * @flow
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   View,
   Text,
+  Button,
+  TextInput,
+  FlatList,
+  SafeAreaView
 } from 'react-native';
 
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { text: '', todos: [] }
+  }
 
-const App = () => {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
+  addTodo = () => {
+    let {text, todos} = this.state
+    this.setState({
+      todos: [...todos, text]
+    })
+
+    this.setState({text: ''})
+  }
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <TextInput
+            style={{height: 40, fontSize: 23, width: 160, borderWidth: 1}}
+            placeholder="Type here..."
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+            />
+
+            <Button title="Add todo" onPress={this.addTodo} />
+
+            <Text>All todos:</Text>
+            <FlatList 
+              data={this.state.todos}
+              renderItem={({item}) => <Text>{item}</Text>}
+              keyExtractor={item=>item}
+            />
+        </View>
+      </SafeAreaView>
+    );
+  }
 };
 
 export default App;
